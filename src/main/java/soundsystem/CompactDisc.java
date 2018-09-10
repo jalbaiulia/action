3364f7e87048c0b9eb1d7565eaca76a03e1298e7
Iulia Jalba <jalba@adobe.com>
2018-09-10 17:27:42 +0300

    first commit

---
+++ src/main/java/soundsystem/CompactDisc.java
@@ -0,0 +1,8 @@
+package soundsystem;
+
+
+public interface CompactDisc {
+
+   void play();
+
+}
---
+++ src/main/java/soundsystem/StgPrppers.java
@@ -0,0 +1,12 @@
+package soundsystem;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class StgPrppers implements CompactDisc {
+
+  public void play() {
+    System.out.println("la la la");
+  }
+}
