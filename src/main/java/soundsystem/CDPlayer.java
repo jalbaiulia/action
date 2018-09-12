cdf063af27712443c93c28bc8c45060aa874f2e4
Iulia Jalba <jalba@adobe.com>
2018-09-12 09:12:20 +0300

    fix this

--- src/main/java/soundsystem/CDPlayer.java
+++ src/main/java/soundsystem/CDPlayer.java
@@ -1,10 +1,30 @@
 package soundsystem;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-@Configuration
-@ComponentScan
-public class CDPlayer {
+@Component
+public class CDPlayer implements MediaPlayer {
 
+  private CompactDisc cd;
+
+  @Autowired
+  public CDPlayer(CompactDisc cd) {
+    this.cd = cd;
+  }
+
+  public void play() {
+    cd.play();
+  }
+
+//
+//  @Autowired
+//  public void setCompactDisc(CompactDisc cd) {
+//    this.cd = cd;
+//  }
+//
+//  @Autowired
+//  public void insertDisc(CompactDisc cd) {
+//    this.cd = cd;
+//  }
 }
--- src/main/java/soundsystem/CDPlayerTest.java
+++ src/main/java/soundsystem/CDPlayerTest.java
@@ -1,8 +1,10 @@
 package soundsystem;
 
 
+import org.junit.Rule;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.runner.RunWith;
@@ -10,9 +12,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = CDPlayer.class)
+@ContextConfiguration(classes = CDPlayerConfig.class)
 public class CDPlayerTest {
+
+//  @Rule
+//  public final SystemOutRule log = new SystemOutRule();
+
+//  @Autowired
+//  private MediaPlayer player;
 
   @Autowired
   private CompactDisc cd;
@@ -22,5 +31,15 @@ public class CDPlayerTest {
     assertNotNull(cd);
   }
 
+  @Test
+  public void play() {
+    cd.play();
+//    assertEquals(
+//        "Playing Sgt. Pepper's Lonely Hearts Club Band" +
+//        " by The Beatles\n",
+//        log.getLog());
+  }
+
 
 }
+
