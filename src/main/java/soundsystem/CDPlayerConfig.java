ae5dfdca6c6560fb5b59ef63fc9330cfe0d0ddf6
Iulia Jalba <jalba@adobe.com>
2018-12-08 12:59:00 +0200

    @ComponentScan

--- src/main/java/soundsystem/CDPlayerConfig.java
+++ src/main/java/soundsystem/CDPlayerConfig.java
@@ -1,8 +1,34 @@
 package soundsystem;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackageClasses={CDPlayer.class})
-public class CDPlayerConfig {}
+public class CDPlayerConfig {
+
+  @Bean
+  public CompactDisc sgtPeppers() {
+    return new SgtPeppers();
+  }
+
+  @Bean
+  public MediaPlayer cdPlayer() {
+    return new CDPlayer(sgtPeppers());
+  }
+
+
+  // By default, all beans in Spring are singletons
+  // Same instance SgtPeppers will be used bu Spring magic, though we have "new SgtPeppers()"
+  @Bean(name="anotherCDPlayer")
+  public CDPlayer anotherCDPlayer() {
+    return new CDPlayer(sgtPeppers());
+  }
+
+
+  @Bean
+  public CDPlayer cdPlayer(CompactDisc compactDisc) {
+    return new CDPlayer(compactDisc);
+  }
+
+}
