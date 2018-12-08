2a3447a0663827316da79ceda222b72dca0ab4c1
Iulia Jalba <jalba@adobe.com>
2018-12-08 12:59:00 +0200

    @ComponentScan

--- src/main/java/soundsystem/CDPlayerConfig.java
+++ src/main/java/soundsystem/CDPlayerConfig.java
@@ -4,5 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackageClasses={CDPlayer.class})
-public class CDPlayerConfig {}
+@ComponentScan
+public class CDPlayerConfig {
+
+}
