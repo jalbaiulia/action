46257e6dc6daa36c9fd1c9c719e09b93042207d0
Iulia Jalba <jalba@adobe.com>
2018-12-08 12:59:00 +0200

    @ComponentScan

--- src/main/java/soundsystem/CDPlayer.java
+++ src/main/java/soundsystem/CDPlayer.java
@@ -3,10 +3,17 @@ package soundsystem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+
+// Injecting a CompactDisc into a CDPlayer bean using autowiring
+
 @Component
 public class CDPlayer implements MediaPlayer {
+
   private CompactDisc cd;
 
+
+  // 1. Constructor Autowire
+  //when Spring creates the CDPlayer bean, it should instantiate it via that constructor and pass in a bean that is assignable to CompactDisc
   @Autowired
   public CDPlayer(CompactDisc cd) {
     this.cd = cd;
@@ -15,5 +22,25 @@ public class CDPlayer implements MediaPlayer {
   public void play() {
     cd.play();
   }
+
+
+
+  // setter Autowire
+  @Autowired
+  public void setCompactDisc(CompactDisc cd) {
+    this.cd = cd;
+  }
+
+
+  // any method Autowire
+       //If there are no matching beans, Spring will throw an exception as the application context is being created.
+       //To avoid that exception, you can set the required attribute on @Autowired to false
+  @Autowired(required=false)
+  public void insertDisc(CompactDisc cd) {
+    this.cd = cd;
+  }
+        //Leaving the property unwired could lead to NullPointer-Exceptions if you don’t check for null in your code.
+
+
 
 }
