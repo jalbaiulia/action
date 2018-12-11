0e036b9cc3137255b0cedd40917d06ace937ab2c
Iulia Jalba <jalba@adobe.com>
2018-12-11 16:45:15 +0200

    Injecting external values

---
+++ src/main/java/soundsystem_2/BlankDisc.java
@@ -0,0 +1,16 @@
+package soundsystem_2;
+
+public class BlankDisc implements CompactDisc {
+
+  private String title;
+  private String artist;
+
+  public BlankDisc(String title, String artist) {
+    this.title = title;
+    this.artist = artist;
+  }
+
+  public void play() {
+    System.out.println("Playing " + title + " by " + artist);
+  }
+}
---
+++ src/main/java/soundsystem_2/ExpressiveConfig.java
@@ -0,0 +1,21 @@
+package soundsystem_2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@PropertySource("classpath:/app.properties")
+public class ExpressiveConfig {
+
+  @Autowired
+  Environment env;
+
+  @Bean
+  public BlankDisc disc() {
+    return new BlankDisc(env.getProperty("disc.title"), env.getProperty("disc.artist"));
+  }
+
+}
